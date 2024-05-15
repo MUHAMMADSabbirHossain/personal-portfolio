3a08453739f598445f292db6145d1f50203b542b
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-05-16 00:52:17 +0600

    react router: package is installed...successfully

---
+++ Personal-Portfolios-FrontEnd/src/routes/roots.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <>hello World</>
+    }
+])
+
+export default router;
