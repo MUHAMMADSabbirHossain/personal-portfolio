29d916569be672e043779bd86c45ac3c3329d92b
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-05-16 08:39:45 +0600

    Header | Navbar: components are added... successfully

--- Personal-Portfolios-FrontEnd/src/layout/Root.jsx
+++ Personal-Portfolios-FrontEnd/src/layout/Root.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 const Root = () => {
     return (
-        <>root
+        <>
+            <Header></Header>
+            <Navbar></Navbar>
             <main>
                 <Outlet></Outlet>
             </main>
