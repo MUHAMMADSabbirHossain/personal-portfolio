1add085b4ea8f75726d205bbfb009c5db3e9ab78
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-05-17 16:12:45 +0600

    home page: landing section edited by adding image... successfully

--- Personal-Portfolios-FrontEnd/src/pages/Home.jsx
+++ Personal-Portfolios-FrontEnd/src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import landingBgImg from "../assets/wavy-black-white-background.jpg";
+
 const Home = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content text-center">
+        <div className="hero min-h-screen" style={{ backgroundImage: `url(${landingBgImg})` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Hello there</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="mb-5 text-5xl font-bold">Welcome to Micro-Services</h1>
+                    <p className="mb-5">Our platform leverages microservices Operating independently, our Tailwind CSS component library, resume builder, and volunteering service ensure seamless integration for web development, career enhancement, and community involvement.
+                    </p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
