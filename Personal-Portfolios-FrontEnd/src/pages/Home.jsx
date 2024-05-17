28d70d312c6317dd4276fc96c1343f0c4283b21b
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-05-17 14:12:46 +0600

    home page: hero edited... successfully

--- Personal-Portfolios-FrontEnd/src/pages/Home.jsx
+++ Personal-Portfolios-FrontEnd/src/pages/Home.jsx
@@ -1,12 +1,10 @@
 const Home = () => {
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Welcome to Micro-Services</h1>
-                    <p className="mb-5">Our platform leverages microservices Operating independently, our Tailwind CSS component library, resume builder, and volunteering service ensure seamless integration for web development, career enhancement, and community involvement.
-                    </p>
+                    <h1 className="text-5xl font-bold">Hello there</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
