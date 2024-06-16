c815fdd72775e4bd69b4d69905a5d1299a61d8b9
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-06-16 12:16:47 +0600

    backend: comment added... successfully

--- Personal-Portfolios-BackEnd/index.js
+++ Personal-Portfolios-BackEnd/index.js
@@ -3,7 +3,9 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+// base url for server
 app.get("/", (req, res) => {
+    console.log(`App get base-url route`);
     res.send(`Personal Portfolios Server is running...`)
 })
 
