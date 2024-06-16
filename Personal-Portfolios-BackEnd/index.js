74ce5739f255a535e29d500fd3c9b15a1849e272
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-06-16 11:58:58 +0600

    backend port | base url: port variable added: base url setup... successfully

--- Personal-Portfolios-BackEnd/index.js
+++ Personal-Portfolios-BackEnd/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.listen("5000", (req, res) => {
-    console.log(`Personal Portfolios is running on: ${5000}`);
+app.get("/", (req, res) => {
+    res.send(`Personal Portfolios Server is running...`)
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Personal Portfolios Server is running on: ${port}`);
 });
