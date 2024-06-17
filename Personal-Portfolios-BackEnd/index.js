1574762a2d9c49112116bbe2fd2a429d8e0060fc
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-06-17 10:09:00 +0600

    backend mongodb | comment | vercel: package installed; connet with user password by .env; commented for vercel and deployed to vercel... successfully

--- Personal-Portfolios-BackEnd/index.js
+++ Personal-Portfolios-BackEnd/index.js
@@ -3,12 +3,41 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+// MongoDB connection string
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ot5ajnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 // base url for server
 app.get("/", (req, res) => {
-    console.log(`App get base-url route`);
-    res.send(`Personal Portfolios Server is running...`)
+    console.log(`Application: base-url get route`);
+    res.send(`Personal Portfolios Server application is running...`)
 })
 
 app.listen(port, (req, res) => {
-    console.log(`Personal Portfolios Server is running on: ${port}`);
+    console.log(`Personal Portfolios Server applicaiton is running on: ${port}`);
 });
