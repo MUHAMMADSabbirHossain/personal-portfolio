2719eb89e8afd623e7828b683a3cf9e66b7ad182
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-06-19 14:41:14 +0600

    backend cors: package installed and express.json() added as middleware

--- Personal-Portfolios-BackEnd/index.js
+++ Personal-Portfolios-BackEnd/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+/* middleware */
+app.use(cors());
+app.use(express.json());
 
 // MongoDB connection string
 const { MongoClient, ServerApiVersion } = require('mongodb');
