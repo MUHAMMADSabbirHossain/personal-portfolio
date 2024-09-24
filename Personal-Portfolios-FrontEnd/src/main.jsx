8f208328b7087b0f27c4b62b17fdbb94ea3ed3c6
Muhammad Sabbir Hossain <dustobalok000.com>
2024-09-24 19:27:09 +0600

    frontend | package: tanstack package installed... successfully.

--- Personal-Portfolios-FrontEnd/src/main.jsx
+++ Personal-Portfolios-FrontEnd/src/main.jsx
@@ -5,11 +5,16 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/roots'
 import AuthProvider from './providers/AuthProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router}></RouterProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
