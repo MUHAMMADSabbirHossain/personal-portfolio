6a826127d6ae765a94a647ef12e12664b82fd1cb
Muhammad Sabbir Hossain Bin Ahmad <dustobalok@gmail.com>
2025-02-03 18:26:19 +0600

    frontend | Volunteering: reverse volunteerings order and comment out console log

--- Personal-Portfolios-FrontEnd/src/pages/Volunteering/Volunteering.jsx
+++ Personal-Portfolios-FrontEnd/src/pages/Volunteering/Volunteering.jsx
@@ -5,7 +5,8 @@ const Volunteering = () => {
 
     const loadedVolunteerings = useLoaderData();
     const volunteerings = loadedVolunteerings;
-    console.log(volunteerings);
+    // console.log(volunteerings);
+    const reversedVolunteerings = [...volunteerings].reverse();
 
     return (
         <div>
@@ -13,12 +14,21 @@ const Volunteering = () => {
 
             <section className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 m-5'>
                 {
-                    volunteerings.map((volunteering) => <div key={volunteering._id}>
+                    reversedVolunteerings.map((volunteering) => <div key={volunteering._id}>
                         <div className="card max-w-96 glass mx-auto  hover:scale-105 ease-in-out duration-300 hover:bg-gray-900 hover:text-white hover:shadow-inner">
                             <figure className="px-5 pt-5"><img src={volunteering?.photoUrl} alt={volunteering?.title} className='shadow-2xl' /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">{volunteering.title} </h2>
                                 <div className="badge badge-secondary font-bold">{volunteering?.address.slice(0, 25)}</div>
+                                {
+                                    (volunteering?.time && volunteering?.time) ?
+                                        <div>
+                                            <time datetime="">at {volunteering?.time}</time>
+                                            <datetime> on {volunteering?.date}</datetime>
+                                        </div> :
+                                        <></>
+                                }
+
                                 <p>{
                                     volunteering?.details.length < 100 ?
                                         volunteering?.details :
